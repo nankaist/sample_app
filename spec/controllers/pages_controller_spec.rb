c995e81dec6d86e47f8228df2889c16f85de0360
chou <nankaist@hotmail.com>
2011-06-14 17:07:00 -0400

    Done with static pages

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,12 +1,20 @@
 require 'spec_helper'
 
 describe PagesController do
+  render_views #tell rspec to test not only the default actions but also the views
 
-  describe "GET 'home'" do
+  describe "GET 'home'" do  #everything in " " are just descriptions for human, not for rspec
     it "should be successful" do
       get 'home'
       response.should be_success
     end
+    
+    it "should have the right title" do
+      get 'home'
+      response.should have_selector("title",
+              :content => "Ruby on Rails Tutorial Sample App | Home")
+    end
+    
   end
 
   describe "GET 'contact'" do
@@ -14,6 +22,27 @@ describe PagesController do
       get 'contact'
       response.should be_success
     end
+    
+    it "should have the right title" do
+      get 'contact'
+      response.should have_selector("title",
+              :content => "Ruby on Rails Tutorial Sample App | Contact")
+    end
+    
   end
-
+  
+  describe "GET 'about'" do
+    it "should be successful" do
+      get 'about'
+      response.should be_success
+    end
+    
+    it "should have the right title" do
+      get 'about'
+      response.should have_selector("title",
+                :content => "Ruby on Rails Tutorial Sample App | About")
+    end
+    
+  end
+    
 end
